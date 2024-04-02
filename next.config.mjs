8ff01269f79f8235d2f935d25a307af4b11489f8
juandavidGF <davad701@gmail.com>
2024-04-02 18:11:12 -0500

    Sentiment Analisys transformersjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Feel free to modify/remove this option
+  webpack: (config) => {
+      config.resolve.alias = {
+          ...config.resolve.alias,
+          "sharp$": false,
+          "onnxruntime-node$": false,
+      }
+      return config;
+  },
+}
 
 export default nextConfig;
