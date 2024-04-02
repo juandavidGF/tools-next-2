0f6f2569a766f2cac42f44156c1fb724625c35ae
juandavidGF <davad701@gmail.com>
2024-04-02 18:54:57 -0500

    asdf

--- app/page.jsx
+++ app/page.jsx
@@ -6,19 +6,15 @@ export default function Home() {
   const [result, setResult] = useState(null);
   const [ready, setReady] = useState(null);
 
-  // Create a reference to the worker object.
   const worker = useRef(null);
 
-  // We use the `useEffect` hook to set up the worker as soon as the `App` component is mounted.
   useEffect(() => {
     if (!worker.current) {
-      // Create the worker if it does not yet exist.
       worker.current = new Worker(new URL('./worker.js', import.meta.url), {
         type: 'module'
       });
     }
 
-    // Create a callback function for messages from the worker thread.
     const onMessageReceived = (e) => {
       switch (e.data.status) {
         case 'initiate':
@@ -33,10 +29,8 @@ export default function Home() {
       }
     };
 
-    // Attach the callback function as an event listener.
     worker.current.addEventListener('message', onMessageReceived);
 
-    // Define a cleanup function for when the component is unmounted.
     return () => worker.current.removeEventListener('message', onMessageReceived);
   });
 
