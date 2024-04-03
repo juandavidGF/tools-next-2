0e989a76042e25e440f7d998ad5cd9dd206f96e2
juandavidGF <davad701@gmail.com>
2024-04-03 17:05:59 -0500

    asd

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
         type: 'module'
       });
     }
-
+    
     const onMessageReceived = (e) => {
       switch (e.data.status) {
         case 'initiate':
@@ -42,8 +42,8 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-12">
-      <h1 className="text-5xl font-bold mb-2 text-center">Transformers.js</h1>
-      <h2 className="text-2xl mb-4 text-center">Next.js template</h2>
+      <h1 className="text-5xl font-bold mb-2 text-center">Sentiment Analysis</h1>
+      {/* <h2 className="text-2xl mb-4 text-center">Next.js template</h2> */}
 
       <input
         className="w-full max-w-xs p-2 border border-gray-300 rounded mb-4"
